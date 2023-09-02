91a218ffbc7a88702d9f27c201e48a5e4a16ddd5
leong <leon.goergen@gmail.com>
2023-09-02 13:27:29 +0200

    updated backend

--- index.js
+++ index.js
@@ -1,47 +1,69 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { body, validationResult } = require('express-validator');
+
 require('dotenv').config();
 
-// Initialize express app
 const app = express();
 
-// Enable CORS and JSON body parsing
+const errorHandler = (err, req, res) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+};
+
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to MongoDB
-mongoose.connect("mongodb+srv://leongoergen:Rpa-suitabilitY@rpa.htm7lfo.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .catch(error => {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
+    });
 
-// Define a schema and model for questionnaire results
 const resultSchema = new mongoose.Schema({
-    questionScores: Object,
+    questionScores: Object,  // Should ideally specify a more detailed schema
     userToken: String,
     timestamp: Date
 });
 const Result = mongoose.model('Result', resultSchema);
 
-// Create an endpoint to store questionnaire results
-app.post('/store-results', (req, res) => {
-    const newResult = new Result({
-        questionScores: req.body.questionScores,
-        userToken: req.body.userToken,
-        timestamp: new Date()
+app.post('/store-results',
+    [
+        body('questionScores').exists().withMessage('questionScores is required'),
+        body('userToken').isString().withMessage('userToken must be a string')
+    ],
+    async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const newResult = new Result({
+                questionScores: req.body.questionScores,
+                userToken: req.body.userToken,
+                timestamp: new Date()
+            });
+
+            await newResult.save();
+            res.json(`Results added: ${newResult}`);
+        } catch (err) {
+            next(err);
+        }
     });
 
-    newResult.save()
-        .then(() => res.json('Results added: ' + newResult))
-        .catch(err => res.status(400).json('Error: ' + err));
+app.get('/all-results', async (req, res, next) => {
+    try {
+        const results = await Result.find();
+        res.json(results);
+    } catch (err) {
+        next(err);
+    }
 });
 
-app.get('/all-results', (req, res) => {
-    Result.find()
-        .then(results => res.json(results))
-        .catch(err => res.status(400).json('Error: ' + err));
-});
+app.use(errorHandler);
 
-// Start the server
-const port = process.env.PORT || 9001
-app.listen(port, () => console.log('Server started on port '  + port));
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Server started on port ${port}`));
